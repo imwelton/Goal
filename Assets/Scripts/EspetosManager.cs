0a8fdae25c40e7ab34a07e6ac93e8cbf0ef985b9
fabsoft-senai <98919802+imwelton@users.noreply.github.com>
2024-10-04 07:38:10 -0300

    Adição dos desafios da cena intermediários 1

---
+++ Assets/Scripts/EspetosManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspetosManager : MonoBehaviour
+{
+    private SliderJoint2D espeto;
+    private JointMotor2D aux;
+
+    private void Start()
+    {
+        espeto = GetComponent<SliderJoint2D>();
+        aux = espeto.motor;
+    }
+
+    private void Update()
+    {
+        if(espeto.limitState == JointLimitState2D.UpperLimit)
+        {
+            aux.motorSpeed = Random.Range(-1, -5);
+            espeto.motor = aux;
+        }
+
+        if (espeto.limitState == JointLimitState2D.LowerLimit)
+        {
+            aux.motorSpeed = Random.Range(1,5);
+            espeto.motor = aux;
+        }
+
+    }
+}
