3cb40b84ea97734c947341325f139d99ca68741f
imwelton <98919802+imwelton@users.noreply.github.com>
2024-10-15 20:10:46 -0300

    LevelManager 03 parte 01

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -24,11 +25,23 @@ public class LevelManager : MonoBehaviour
             GameObject btnNovo = Instantiate(botao) as GameObject;
             BotaoLevel btnNew = btnNovo.GetComponent<BotaoLevel>();
             btnNew.levelTxtBtn.text = level.levelText;
+
+            if (PlayerPrefs.GetInt("Level" + btnNew.levelTxtBtn.text) == 1)
+            {
+                level.desbloqueado = 1;
+                level.habilitado = true;
+            }
+
             btnNew.desbloqueadoBtn = level.desbloqueado;
             btnNew.GetComponent<Button>().interactable = level.habilitado;
 
             btnNovo.transform.SetParent(localBtn, false);
         }
+    }
+
+    void ClickLevel(string level)
+    {
+        SceneManager.LoadScene(level);
     }
 
     private void Start()
