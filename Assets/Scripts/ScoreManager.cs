1b28921a8befad426f0693bc7c7c1ef8b9badde4
fabsoft-senai <98919802+imwelton@users.noreply.github.com>
2024-10-19 13:34:40 -0300

    Criação do ScoreManager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public int moedas;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GameStartScoreM()
+    {
+        if (PlayerPrefs.HasKey("moedasSave"))
+        {
+            moedas = PlayerPrefs.GetInt("moedasSave");
+        }
+        else
+        {
+            moedas = 100;
+            PlayerPrefs.SetInt("moedasSave", moedas);
+        }
+    }
+
+    public void UpdateScore()
+    {
+        moedas = PlayerPrefs.GetInt("moedasSave");
+    }
+
+    public void ColetaMoedas(int coin)
+    {
+        moedas += coin;
+        SalvaMoedas(coin);
+    }
+
+    public void PerdeMoedas(int coin)
+    {
+        moedas -= coin;
+        SalvaMoedas(coin);
+    }
+
+    public void SalvaMoedas(int coin)
+    {
+        PlayerPrefs.SetInt("moedasSave", coin);
+    }
+
+}
