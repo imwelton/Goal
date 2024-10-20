df394bcc502549d45d26e9474d19d98cf1da9031
fabsoft-senai <98919802+imwelton@users.noreply.github.com>
2024-10-20 14:47:01 -0300

    Bola Control

--- Assets/Scripts/BolaControl.cs
+++ Assets/Scripts/BolaControl.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
-public class Rotacao : MonoBehaviour
+
+public class BolaControl : MonoBehaviour
 {
     [SerializeField] private Transform posStart;
     [SerializeField] private Image setaImg;
@@ -12,9 +14,26 @@ public class Rotacao : MonoBehaviour
     public bool liberaRot = false;
     public bool liberaTiro = false;
 
+    private Rigidbody2D bola;
+    public float force = 0;
+    public Image seta2Img;
+
+    private void Awake()
+    {
+        setaImg = GameObject.Find("SetaCinza").GetComponent<Image>();
+        setaGO = GameObject.Find("CanvasSeta");
+        setaGO.SetActive(false);
+    }
+
     private void Start()
     {
-        PosicionaBola();       
+        posStart = GameObject.Find("PosStart").GetComponent<Transform>();
+        PosicionaBola();
+
+        //força
+
+        seta2Img = setaImg.gameObject.transform.GetChild(0).GetComponent<Image>();
+        bola = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -23,7 +42,12 @@ public class Rotacao : MonoBehaviour
         InputDeRotacao();
         LimitaRotacao();
         PosicionaSeta();
+
+        //força
+        ControlaForca();
+        AplicaForca();
     }
+
     private void PosicionaSeta()
     {
         setaImg.rectTransform.position = transform.position;
@@ -46,14 +70,14 @@ public class Rotacao : MonoBehaviour
             float moveY = Input.GetAxis("Mouse Y");
 
 
-            if(zRotate < 90)
+            if (zRotate < 90)
             {
                 if (moveY > 0)
                 {
                     zRotate += 2.5f;
                 }
             }
-            if(zRotate > 0)
+            if (zRotate > 0)
             {
                 if (moveY < 0)
                 {
@@ -65,10 +89,11 @@ public class Rotacao : MonoBehaviour
 
     void LimitaRotacao()
     {
-        if(zRotate > 90)
+        if (zRotate > 90)
         {
             zRotate = 90;
-        }else if(zRotate < 0)
+        }
+        else if (zRotate < 0)
         {
             zRotate = 0;
         }
@@ -86,5 +111,37 @@ public class Rotacao : MonoBehaviour
         liberaTiro = true;
         setaGO.SetActive(false);
         AudioManager.instance.SonsFXToca(1);
+    }
+
+    void AplicaForca()
+    {
+        float x = force * Mathf.Cos(zRotate * Mathf.Deg2Rad);
+        float y = force * Mathf.Sin(zRotate * Mathf.Deg2Rad);
+
+        if (liberaTiro)
+        {
+            bola.AddForce(new Vector2(x, y));
+            liberaTiro = false;
+        }
+    }
+
+    void ControlaForca()
+    {
+        if (liberaRot)
+        {
+            float moveX = Input.GetAxis("Mouse X");
+
+            if (moveX < 0)
+            {
+                seta2Img.fillAmount += 0.8f * Time.deltaTime;
+                force = seta2Img.fillAmount * 1000;
+            }
+            if (moveX > 0)
+            {
+                seta2Img.fillAmount -= 0.8f * Time.deltaTime;
+                force = seta2Img.fillAmount * 1000;
+            }
+
+        }
     }
 }
